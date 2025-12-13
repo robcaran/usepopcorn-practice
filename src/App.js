4d847c746ef9734032f574b457abce215c0632b8
Robert Caran <rcaran@asu.edu>
2025-12-13 15:44:01 -0700

    refactor: consolidate watched movies state management and improve component structure

--- src/App.js
+++ src/App.js
@@ -54,6 +54,7 @@ const average = (arr) =>
 // ? STRUCTURAL COMPONENT
 export default function App() {
   const [movies, setMovies] = useState(tempMovieData);
+  const [watched, setWatched] = useState(tempWatchedData);
 
   return (
     <>
@@ -63,8 +64,13 @@ export default function App() {
         <NumResults movies={movies} />
       </NavBar>
       <Main>
-        <ListBox> <MovieList movies={movies}/> </ListBox>
-        <WatchedBox />
+        <Box>
+          <MovieList movies={movies} />
+        </Box>
+        <Box>
+          <WatchedSummary watched={watched} />
+          <WatchedMoviesList watched={watched} />
+        </Box>
       </Main>
     </>
   );
@@ -119,18 +125,15 @@ function Main({ children }) {
 }
 
 // ? STATEFUL C.
-function ListBox({ children }) {
-  const [isOpen1, setIsOpen1] = useState(true);
+function Box({ children }) {
+  const [isOpen, setIsOpen] = useState(true);
 
   return (
     <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen1((open) => !open)}
-      >
-        {isOpen1 ? "–" : "+"}
+      <button className="btn-toggle" onClick={() => setIsOpen((open) => !open)}>
+        {isOpen ? "–" : "+"}
       </button>
-      {isOpen1 && {children} />}
+      {isOpen && children}
     </div>
   );
 }
@@ -159,29 +162,6 @@ function Movie({ movie }) {
         </p>
       </div>
     </li>
-  );
-}
-
-// ? STATEFUL C.
-function WatchedBox() {
-  const [watched, setWatched] = useState(tempWatchedData);
-  const [isOpen2, setIsOpen2] = useState(true);
-
-  return (
-    <div className="box">
-      <button
-        className="btn-toggle"
-        onClick={() => setIsOpen2((open) => !open)}
-      >
-        {isOpen2 ? "–" : "+"}
-      </button>
-      {isOpen2 && (
-        <>
-          <WatchedSummary watched={watched} />
-          <WatchedMoviesList watched={watched} />
-        </>
-      )}
-    </div>
   );
 }
 
