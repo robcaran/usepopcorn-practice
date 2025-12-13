63ebaab9efba948bdf0b3d044bd263ef295d5ccf
Robert Caran <rcaran@asu.edu>
2025-12-13 15:14:44 -0700

    refactor: use component composition to avoid prop drilling

--- src/App.js
+++ src/App.js
@@ -62,12 +62,16 @@ export default function App() {
         <Search />
         <NumResults movies={movies} />
       </NavBar>
-      <Main movies={movies} />
+      <Main>
+        <ListBox> <MovieList movies={movies}/> </ListBox>
+        <WatchedBox />
+      </Main>
     </>
   );
 }
 
 // ? STRUCTURAL COMPONENT
+// NavBar gets three components passed in via children
 function NavBar({ children }) {
   return <nav className="nav-bar">{children}</nav>;
 }
@@ -110,17 +114,12 @@ function NumResults({ movies }) {
 
 // ? STRUCTURAL C.
 // * MAIN
-function Main({ movies }) {
-  return (
-    <main className="main">
-      <ListBox movies={movies} />
-      <WatchedBox />
-    </main>
-  );
+function Main({ children }) {
+  return <main className="main">{children}</main>;
 }
 
 // ? STATEFUL C.
-function ListBox({ movies }) {
+function ListBox({ children }) {
   const [isOpen1, setIsOpen1] = useState(true);
 
   return (
@@ -131,7 +130,7 @@ function ListBox({ movies }) {
       >
         {isOpen1 ? "–" : "+"}
       </button>
-      {isOpen1 && <MovieList movies={movies} />}
+      {isOpen1 && {children} />}
     </div>
   );
 }
