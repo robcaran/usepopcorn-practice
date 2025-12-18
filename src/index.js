fdb6c07f9946230f16141e29e120d829bd593cb2
Robert Caran <rcaran@asu.edu>
2025-12-18 13:06:21 -0700

    add onSetRating callback to StarRating for parent sync

--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
-import React from "react";
+import React, { use, useState } from "react";
 import ReactDOM from "react-dom/client";
 // import './index.css';
 // import App from './App';
 import StarRating from "./StarRating";
+
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+
+  return (
+    <div>
+      <StarRating color="blue" maxRating={10} onSetRating={setMovieRating} />
+      <p>This movie was rated {movieRating} stars</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -12,12 +23,7 @@ root.render(
       maxRating={6}
       messages={["Terrible", "Bad", "Ok", "Good", "Excellent"]}
     />
-    <StarRating
-      maxRating={24}
-      size={36}
-      color="green"
-      className="test"
-      defaultRating={3}
-    />
+    <StarRating size={36} color="green" className="test" defaultRating={3} />
+    <Test />
   </React.StrictMode>
 );
